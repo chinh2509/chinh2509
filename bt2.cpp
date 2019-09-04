da09d506f3d766c4b5dbcbc4e9082b800467e9a7
chinh2509 <54590985+chinh2509@users.noreply.github.com>
2019-09-05 00:14:18 +0700

    nộp bài tập

---
+++ bt2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int a;
+	scanf("%d",&a);
+	if (a>0){ 
+		printf("so duong");}
+	else{
+		printf("so âm");
+		}
+	return 0;
+	}	
+	
+	
---
+++ bt3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+	int a,b,x;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	a=x;
+	b=a;
+	a=b;
+	printf ("x=%d\n",a);
+	printf ("y=%d\n",b);
+	
+	return 0;
+	}	
+	
