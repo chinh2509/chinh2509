541ffe697140d9ca0b10ab94bd755691aa425d79
chinh2509 <54590985+chinh2509@users.noreply.github.com>
2019-09-06 14:22:00 +0700

    Create baitapvnloop1.cpp

---
+++ baitapvnloop1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int a,i=1;
+	scanf ("%d",&a);
+    while(i<a){
+
+    	i++;
+    if(i%2==0){
+    printf("%d\n",i);
+    }}
+    return 0;
+    
+    }
+   	
+    	
+	
