2df096f773cac2cdc736d393e476a2da944d5093
chinh2509 <54590985+chinh2509@users.noreply.github.com>
2019-09-08 17:42:53 +0700

    btvn

---
+++ cho20soinrasole.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int can[20],i=0;
+	for(int i=0;i<20;i++)
+	{scanf("%d",&can[i]);}
+	if(can[i]%2!=0)
+	{
+	for(int i=0;i<20;i++)
+	{printf("cac so le la:%d\n",can[i]);
+	}}
+	return 0;
+	}
---
+++ tinhtrungbinhcong.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int can[12],tongtb;
+	for(int i=0;i<12;i++)
+	{scanf("%d",&can[i]);
+	
+	}
+		tongtb=(can[0]+can[1]+can[2]+can[3]+can[4]+can[5]+can[6]+can[7]+can[8]+can[9]+can[10]+can[11])/12;
+	printf("tongtb=%d",tongtb);
+	
+	return 0;
+	}
