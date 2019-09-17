2e848ebd3f0405b3dbb53bfbc9fb9d6d75a5ec43
chinh2509 <54590985+chinh2509@users.noreply.github.com>
2019-09-17 12:02:33 +0700

    bt mảng và chuỗi

---
+++ BVN_MANG.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+	int a[4][5];
+	for(int i=0;i<4;i++){
+		
+	for(int j=0;j<5;j++){
+		scanf("%d",&a[i][j]);
+		printf("%4d",a[i][j]);
+	}printf("\n");
+}
+    int str[6];
+    scanf("%s",str);
+    for(int i=0;i<4;i++)
+    {for(int j=0;j<5;j++)
+    {if(str[6]=a[i][j]){
+    		
+    	printf("day str co trong mang");
+    }else{
+    	printf("day str khong co mang");
+        }	
+    }printf("\n");
+}
+
+	
+	
+
+	
+	
+	return 0;
+	}
