34cfbd82ad12f8fba32ecd943375e102ccfc4178
chinh2509 <54590985+chinh2509@users.noreply.github.com>
2019-09-19 15:54:05 +0700

    bt 1

---
+++ Viet_hoa.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int void lowerCase(char s[]);
+    for(int i = 0; i >=0 ; --i)
+	{
+		if(s[i]>=97 && s[i]<=122){
+		
+		}else{
+			printf("%c",s[i]+32);
+		}
+		
+	} 
+	  
+int main(){
+	
+	char s[50];
+	printf("nhap mang;");
+	scanf("%s",s);
+	void lowerCase;
+	  
+	return 0;
+	}	
