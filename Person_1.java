9b364f191ee0f9d00911c8ff3449d7a00686e82a
chinh2509 <54590985+chinh2509@users.noreply.github.com>
2020-01-16 11:33:29 +0700

    bt1 java

---
+++ Person_1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import person.person;
+
+/**
+ *
+ * @author Dell
+ */
+class Person extends person {
+
+    public Person() {
+    }
+    
+}
