0c857bfda3293792537c39e0e7f7a2e1639293dd
chinh2509 <54590985+chinh2509@users.noreply.github.com>
2020-02-22 19:57:44 +0700

    car

---
+++ testcar.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Car;
+
+import SoccerPlayer.SocerPlayer;
+import person.person;
+
+/**
+ *
+ * @author Dell
+ */
+public class testCar {
+    public static void main(String[] args) {
+         //khơi tạo đối tượng
+        SocerPlayer SocerPlayer = new SocerPlayer();
+   
+        //gọi phương thức
+        person.pack();
+        person.accelerate(); 
+        person.move();
+        
+}
+    }
