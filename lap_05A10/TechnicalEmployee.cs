69a773d7d3a0bbdb7040c0258e1848becc6a6dd1
chinh2509 <54590985+chinh2509@users.noreply.github.com>
2020-05-22 10:47:12 +0700

    lap_05

---
+++ lap_05A10/TechnicalEmployee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class TechnicalEmployee : Employee
+    {
+        public int successfullCheckIns = 5;
+
+        public TechnicalEmployee(String name) : base(name, 7000)
+        {
+
+        }
+        public override String employeeStatus()
+        {
+            return this.toString() + "has" + this.successfullCheckIns + "successfull check ins";
+        }
+    }
+}
