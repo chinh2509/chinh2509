1a4684e1f220a3c569cde817a64d638556727196
chinh2509 <54590985+chinh2509@users.noreply.github.com>
2020-10-03 02:13:25 +0700

    Lab1

---
+++ Templates/Lab01/Models.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Templates.Models;
+
+namespace Lab01
+{
+    internal class Models
+    {
+        internal class ProductContext
+        {
+            public ProductContext()
+            {
+            }
+
+            public IQueryable<Product> Products { get; internal set; }
+        }
+    }
+}
---
+++ Templates/Models/ProductContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Templates.Models;
+
+namespace WingtipToys.Models
+{
+  
+    
+        public class ProductContext : DbContext
+        {
+            public ProductContext() : base("WingtipToys")
+            {
+            }
+            public DbSet<Category> Categories { get; set; }
+            public DbSet<Product> Products { get; set; }
+        }
+    
+}
